34765ea447b3b07c806d1dea518c8abe628cd6c4
johnysantos22 <santosjohny302@gmail.com>
2025-01-01 17:29:43 -0300

    brincando com os codigos

---
+++ script.js
@@ -0,0 +1,10 @@
+function ligar() {
+const img = document.getElementById("img");
+img.classList.add("acessa");
+}
+
+function desligar() {
+    const img = document.getElementById("img");
+    img.classList.remove("acessa");
+    
+}
